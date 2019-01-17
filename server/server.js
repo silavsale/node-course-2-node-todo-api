20dddc1b7c30b74ea58934ae4a300080fb2540ba
Silavsale <silavsale@gmail.com>
2019-01-17 17:33:55 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
+
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -30,7 +32,28 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
 
+// GET /todos/12345
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(id);
+
+    if (!ObjectID.isValid(id)) {
+        console.log("Status 404");
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
 
 });
 
