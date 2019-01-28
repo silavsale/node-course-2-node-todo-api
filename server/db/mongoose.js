1bbd2f4aa5cf195aadf2467f4d5276c769e5e5a9
Silavsale <silavsale@gmail.com>
2019-01-27 23:50:58 -0500

    Update config to use json file and set heroku variables

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,9 +4,9 @@ mongoose.set('useCreateIndex', true);
 
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-// mongoose.connect(process.env.MONGODB_URI = 'mongodb://admin:admin12@ds143604.mlab.com:43604/todoapp', {useNewUrlParser: true});
+mongoose.connect(process.env.MONGODB_URI = 'mongodb://admin:admin12@ds143604.mlab.com:43604/todoapp', {useNewUrlParser: true});
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+// mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
 
 
 module.exports = {mongoose};
